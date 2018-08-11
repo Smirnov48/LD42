2edfdc6c5fdaf408db6e423b880e0dfc40964182
Alexander Smirnov <smirnov48@gmail.com>
2018-08-11 20:11:31 +0300

    aaaaaa!h!!!

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ new Phaser.Game({
 	height:360, 
 	type:Phaser.AUTO,
 	physics: {
-		default: 'matter',
-		matter: {
+		default: 'arcade',
+		arcade: {
 			gravity: {
 				y: 300
 			},
--- js/scenes/game.js
+++ js/scenes/game.js
@@ -5,21 +5,25 @@ class Game extends Phaser.Scene {
 	}
 
 	preload() {
-		this.load.image('block', 'assets/block.png')
+		this.load.image('block', 'assets/block.png');
+		this.load.image('player', 'assets/player.png');
 	}
 
 	create () {
-//		this.cameras.main.setBounds(0, -1000, 640, 1360);
-//		this.physics.world.setBounds(0, -1000, 640, 1360);
+		this.cameras.main.setBounds(0, -1000, 640, 1360);
+		this.physics.world.setBounds(0, -1000, 640, 1360);
 //		this.physics.world.OVERLAP_BIAS = 8;
-		this.matter.world.setBounds(0, -1000, 640, 1360, 32, true, true, false, true);
+//		this.matter.world.setBounds(0, -1000, 640, 1360, 32, true, true, false, true);
 
-//		var group = this.physics.add.group();
-		for (var i = 0; i < 150; i++) {
-//			group.create(Math.random() * 600, Math.random() * -900, 'block').setCollideWorldBounds(true);
-		    this.matter.add.image(Math.random() * 600, Math.random() * -900, 'block');
+		var group = this.physics.add.group();
+		for (var i = 0; i < 50; i++) {
+			group.create(Math.random() * 600, Math.random() * -900, 'block').setCollideWorldBounds(true);
+//		    this.matter.add.image(Math.random() * 600, Math.random() * -900, 'block');
 		}
-//		this.physics.add.collider(group);
+		this.physics.add.collider(group);
+
+		var player = this.physics.add.image(100, 100, 'player').setCollideWorldBounds(true);
+		this.physics.add.collider(player, group);
 	}
 
 }
