1945db5cb8b2fe75e4260452eab1ac81f95afb15
Alexander Smirnov <smirnov48@gmail.com>
2018-08-11 16:34:55 +0300

    Arcade physics

--- js/scenes/game.js
+++ js/scenes/game.js
@@ -9,11 +9,17 @@ class Game extends Phaser.Scene {
 	}
 
 	create () {
+		this.cameras.main.setBounds(0, -1000, 640, 1360);
+		this.physics.world.setBounds(0, -1000, 640, 1360);
+		this.physics.world.OVERLAP_BIAS = 8;
+//		this.matter.world.setBounds(0, -1000, 640, 1360, 32, true, true, false, true);
+
 		var group = this.physics.add.group();
-		for (var i = 0; i < 200; i++) {
-			group.create(Math.random() * 600, Math.random() * 100, 'block').setCollideWorldBounds(true);
+		for (var i = 0; i < 50; i++) {
+			group.create(Math.random() * 600, Math.random() * -900, 'block').setCollideWorldBounds(true);
+//		    this.matter.add.image(Math.random() * 600, Math.random() * -900, 'block');
 		}
-		this.physics.add.collider(group, group);
+		this.physics.add.collider(group);
 	}
 
 }
