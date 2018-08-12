1db8f8328f1c91530b24129d55fafc55c13cf0b8
Alexander Smirnov <smirnov48@gmail.com>
2018-08-12 15:11:44 +0300

    random manipulator

--- js/manipulator.js
+++ js/manipulator.js
@@ -12,6 +12,7 @@ class Manipulator {
 		this.velocity = 4;
 
 		this.catchedBlock = null;
+		this.whereDrop = null;
 	}
 
 	update (time, delta) {
@@ -19,30 +20,25 @@ class Manipulator {
 
 		if (time > this.curTime) {
 			this.curTime = time + 500 + Math.random() * 3000;
-
-			this.needReleaseBlock = true;
 		}
 
 		if (this.manipulator.x > 707 || this.manipulator.x < -33) {
 			this.velocity = -this.velocity;
 			if (!this.catchedBlock) {
 				this.catchedBlock = new Block(this.game, this.manipulator.x, this.manipulator.y + 16);
+				this.whereDrop = Math.floor(Math.random() * 18);
 			}
 		}
 
 		if (
-			this.needReleaseBlock && 
-			this.manipulator.x % 32 == 0 &&
-			this.manipulator.x > 0 &&
-			this.manipulator.x < 640
-		){
-			if (this.catchedBlock) {
-				this.catchedBlock.setPosition(this.manipulator.x, this.manipulator.y + 24);
-				this.blockPool.add(this.catchedBlock);
-				this.catchedBlock = false;
-			}
-
-			this.needReleaseBlock = false;
+			this.catchedBlock && 
+			this.manipulator.x % 32 == 0 && 
+			Math.floor(this.manipulator.x / 32) == this.whereDrop
+		) {
+			this.catchedBlock.setPosition(this.manipulator.x, this.manipulator.y + 24);
+			this.blockPool.add(this.catchedBlock);
+			this.catchedBlock = false;
+			this.whereDrop = null
 		}
 
 		if (this.catchedBlock) {
