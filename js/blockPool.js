8d7f7f77a286090fb800c41abc1ba48e1ba4ea08
Alexander Smirnov <smirnov48@gmail.com>
2018-08-12 18:57:12 +0300

    collide in progress

--- js/blockPool.js
+++ js/blockPool.js
@@ -32,4 +32,21 @@ class BlockPool {
 		return false;
 	}
 
+	collidePlayer(player) {
+		for (let block of this.blocks) {
+			let rect1 = player.sprite;
+			let rect2 = block.block;
+
+			if (
+				rect1.x < rect2.x + rect2.width &&
+				rect1.x + rect1.width > rect2.x &&
+				rect1.y < rect2.y + rect2.height &&
+				rect1.height + rect1.y > rect2.y
+			) {
+				return block;
+			}
+		}	
+		return false;	
+	}
+
 }
--- js/player.js
+++ js/player.js
@@ -11,22 +11,35 @@ class Player {
 
 	update(time, delta){
 
+		let collideObject = this.blockPool.collidePlayer(this);
+
 		if (this.keys.W.isDown && this.sprite.y >= 326) {
-			this.sprite.setVelocityY(-200);
+			this.sprite.setVelocityY(-210);
 		}
 		if (this.keys.S.isDown) {
 
 		}
 		if (this.keys.A.isDown) {
 			this.sprite.play('run', true);
-			this.sprite.x -= 2;			
+			this.sprite.x -= 2;
+			if (collideObject) {
+				this.sprite.x += 2;
+			}
 			this.sprite.setFlipX(true);
 		} 
 		if (this.keys.D.isDown) {
 			this.sprite.play('run', true);
 			this.sprite.x += 2;
+			if (collideObject) {
+				this.sprite.x -= 2;
+			}
 			this.sprite.setFlipX(false);
 		} 		
+
+		if (collideObject) {
+			this.sprite.y = collideObject.y - 16;
+		}
+
 		if (
 			!this.keys.W.isDown &&
 			!this.keys.A.isDown &&
