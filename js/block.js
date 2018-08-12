e9b471c9062917dd237dc09c3ecfb93fd2428fe7
Alexander Smirnov <smirnov48@gmail.com>
2018-08-12 13:58:49 +0300

    some changes

--- js/block.js
+++ js/block.js
@@ -1,12 +1,17 @@
 class Block {
 
-	constructor(game, x, y) {
+	constructor(game, x, y, blocksPool) {
+		this.blocksPool = blocksPool;
 		this.game = game;
 		this.block = game.add.image(x, y, 'block');
 	}
 
 	update (time, delta) {
 		this.block.y += 2;
+		let collideBlock = this.blocksPool.getCollideObject(this);
+		if (this.block.y > 344) { 
+			this.block.y = 344;
+		}
 	}
 
 }
--- js/blocksPool.js
+++ js/blocksPool.js
@@ -6,13 +6,21 @@ class BlockPool {
 	}
 
 	createBlock (x, y) {
-		let block = new Block(this.game, x, y);
+		let block = new Block(this.game, x, y, this);
 		this.blocks.push(block);
 	}
 
 	update (time, delta) {
 		for (let block of this.blocks) {
-			block.update(time, delta);
+			block.update(time, delta, this);
+		}
+	}
+
+	getCollideObject (currentBlock) {
+		for (let block of this.blocks) {
+			if (block == currentBlock) {
+			//console.log("!");
+			}
 		}
 	}
 
